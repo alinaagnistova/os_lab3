e73eaebf80eb6877bb41156ae9688fb34a896ad2
alinaagnistova <agnistova1@gmail.com>
2025-01-14 18:55:57 +0000

    done

--- kernel/ioctl_custom.c
+++ kernel/ioctl_custom.c
@@ -1,85 +1,120 @@
-#include <signal.h>
-#include <asm-generic/errno-base.h>
-#include <linux/init.h>
 #include <linux/module.h>
+#include <linux/kernel.h>
 #include <linux/fs.h>
-#include <linux/ioctl.h>
-#include <linux/sched.h>
 #include <linux/uaccess.h>
 #include <linux/pid.h>
+#include <linux/sched.h>
+#include <linux/sched/types.h>
 #include <linux/sched/task.h>
-#include <linux/fs_struct.h>
+#include <linux/init.h>
 #include <linux/dcache.h>
+#include <linux/path.h>
+#include <linux/fs_struct.h>
 
 #define DEVICE_NAME "task_info"
-#define IOCTL_GET_TASK_CPUTIME _IOR('t', 1, struct task_cputime)
-#define IOCTL_GET_INODE_INFO  _IOR('t', 2, unsigned long)
 
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Student");
-MODULE_DESCRIPTION("IOCTL example for task_cputime and inode info");
+/*
+ * Определяем собственные команды IOCTL
+ */
+#define IOCTL_GET_TASK_CPUTIME _IOWR('t', 1, struct param_task_cputime)
+#define IOCTL_GET_INODE_INFO   _IOWR('t', 2, struct param_inode_info)
+
+struct my_task_cputime {
+    __u64 utime;
+    __u64 stime;
+    __u64 sum_exec_runtime;
+};
 
 struct inode_info {
     unsigned long inode_number;
     unsigned long size;
 };
 
+
+struct param_task_cputime {
+    pid_t pid;
+    struct my_task_cputime t_cputime;
+};
+
+struct param_inode_info {
+    pid_t pid;
+    struct inode_info i_info;
+};
+
 static int device_open(struct inode *inode, struct file *file) {
-    printk(KERN_INFO "Device opened\n");
+    printk(KERN_INFO "Device %s opened\n", DEVICE_NAME);
     return 0;
 }
 
 static int device_release(struct inode *inode, struct file *file) {
-    printk(KERN_INFO "Device closed\n");
+    printk(KERN_INFO "Device %s closed\n", DEVICE_NAME);
     return 0;
 }
 
 static long device_ioctl(struct file *file, unsigned int cmd, unsigned long arg) {
-    struct task_struct *task;
-    struct task_cputime t_cputime;
-    struct inode_info i_info;
-    pid_t pid;
+    struct task_struct *task = NULL;
 
     switch (cmd) {
-        case IOCTL_GET_TASK_CPUTIME:
-            if (copy_from_user(&pid, (pid_t __user *)arg, sizeof(pid_t))) {
+
+        case IOCTL_GET_TASK_CPUTIME: {
+            struct param_task_cputime param;
+            if (copy_from_user(&param,
+                               (struct param_task_cputime __user *)arg,
+                               sizeof(param))) {
+                return -EFAULT;
+            }
+            task = get_pid_task(find_get_pid(param.pid), PIDTYPE_PID);
+            if (!task) {
+                printk(KERN_ERR "IOCTL_GET_TASK_CPUTIME: Task %d not found\n",
+                       param.pid);
+                return -ESRCH;
+            }
+            param.t_cputime.utime = task->utime;
+            param.t_cputime.stime = task->stime;
+            param.t_cputime.sum_exec_runtime = task->se.sum_exec_runtime;
+
+            put_task_struct(task);
+
+            if (copy_to_user((struct param_task_cputime __user *)arg,
+                             &param,
+                             sizeof(param))) {
+                return -EFAULT;
+            }
+            break;
+        }
+
+        case IOCTL_GET_INODE_INFO: {
+            struct param_inode_info param;
+            if (copy_from_user(&param,
+                               (struct param_inode_info __user *)arg,
+                               sizeof(param))) {
                 return -EFAULT;
             }
 
-            task = get_pid_task(find_get_pid(pid), PIDTYPE_PID);
+            task = get_pid_task(find_get_pid(param.pid), PIDTYPE_PID);
             if (!task) {
-                printk(KERN_ERR "Task not found\n");
+                printk(KERN_ERR "IOCTL_GET_INODE_INFO: Task %d not found\n",
+                       param.pid);
                 return -ESRCH;
             }
 
-            thread_group_cputime_adjusted(task, &t_cputime);
-
-            if (copy_to_user((struct task_cputime __user *)arg, &t_cputime, sizeof(struct task_cputime))) {
-                put_task_struct(task);
-                return -EFAULT;
+            {
+                struct path pwd;
+                get_fs_pwd(task->fs, &pwd);
+                param.i_info.inode_number = pwd.dentry->d_inode->i_ino;
+                param.i_info.size = pwd.dentry->d_inode->i_size;
+                path_put(&pwd);
             }
 
             put_task_struct(task);
-            break;
 
-        case IOCTL_GET_INODE_INFO:
-            if (copy_from_user(&pid, (pid_t __user *)arg, sizeof(pid_t))) {
+            if (copy_to_user((struct param_inode_info __user *)arg,
+                             &param,
+                             sizeof(param))) {
                 return -EFAULT;
-            }
-
-            task = get_pid_task(find_get_pid(pid), PIDTYPE_PID);
-            if (!task ⠞⠺⠺⠵⠞⠞⠟⠞⠵⠟⠞ !task->fs->pwd.dentry || !task->fs->pwd.dentry->d_inode) {
-                printk(KERN_ERR "Invalid inode structure\n");
-                return -EINVAL;
-            }
-
-            i_info.inode_number = task->fs->pwd.dentry->d_inode->i_ino;
-            i_info.size = task->fs->pwd.dentry->d_inode->i_size;
-
-            if (copy_to_user((struct inode_info __user *)arg, &i_info, sizeof(struct inode_info))) {
-                return -EFAULT;
-            }
+                             }
             break;
+        }
 
         default:
             return -ENOTTY;
@@ -89,29 +124,35 @@ static long device_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 }
 
 static struct file_operations fops = {
-    .owner = THIS_MODULE,
+    .owner          = THIS_MODULE,
     .unlocked_ioctl = device_ioctl,
-    .open = device_open,
-    .release = device_release,
+    .open           = device_open,
+    .release        = device_release,
 };
 
-static int __init ioctl_example_init(void) {
+/*
+ * Инициализация и выгрузка модуля
+ */
+static int __init ioctl_custom_init(void) {
     int ret;
-
     ret = register_chrdev(0, DEVICE_NAME, &fops);
     if (ret < 0) {
-        printk(KERN_ERR "Failed to register device\n");
+        printk(KERN_ERR "Failed to register device '%s'\n", DEVICE_NAME);
         return ret;
     }
-
-    printk(KERN_INFO "Module loaded with device major number %d\n", ret);
+    printk(KERN_INFO "Module '%s' loaded with device major number %d\n",
+           DEVICE_NAME, ret);
     return 0;
 }
 
-static void __exit ioctl_example_exit(void) {
+static void __exit ioctl_custom_exit(void) {
     unregister_chrdev(0, DEVICE_NAME);
-    printk(KERN_INFO "Module unloaded\n");
+    printk(KERN_INFO "Module '%s' unloaded\n", DEVICE_NAME);
 }
 
-module_init(ioctl_example_init);
-module_exit(ioctl_example_exit);
+module_init(ioctl_custom_init);
+module_exit(ioctl_custom_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Student");
+MODULE_DESCRIPTION("Custom IOCTL kernel module");
--- user/main-user.c
+++ user/main-user.c
@@ -1,3 +1,83 @@
-//
-// Created by vboxuser on 1/12/25.
-//
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <linux/types.h>
+
+#define DEVICE_PATH "/dev/task_info"
+
+struct my_task_cputime {
+    __u64 utime;
+    __u64 stime;
+    __u64 sum_exec_runtime;
+};
+
+struct inode_info {
+    unsigned long inode_number;
+    unsigned long size;
+};
+
+#define IOCTL_GET_TASK_CPUTIME  _IOWR('t', 1, struct param_task_cputime)
+#define IOCTL_GET_INODE_INFO    _IOWR('t', 2, struct param_inode_info)
+
+struct param_task_cputime {
+    pid_t pid;
+    struct my_task_cputime t_cputime;
+};
+
+struct param_inode_info {
+    pid_t pid;
+    struct inode_info i_info;
+};
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <PID> <mode (1=cputime, 2=inode)>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    pid_t pid = atoi(argv[1]);
+    int mode  = atoi(argv[2]);
+
+    int fd = open(DEVICE_PATH, O_RDWR);
+    if (fd < 0) {
+        perror("Failed to open device");
+        return EXIT_FAILURE;
+    }
+
+    if (mode == 1) {
+        struct param_task_cputime param;
+        param.pid = pid;
+        if (ioctl(fd, IOCTL_GET_TASK_CPUTIME, &param) == -1) {
+            perror("IOCTL_GET_TASK_CPUTIME failed");
+            close(fd);
+            return EXIT_FAILURE;
+        }
+        printf("User time:            %llu\n", (unsigned long long)param.t_cputime.utime);
+        printf("System time:          %llu\n", (unsigned long long)param.t_cputime.stime);
+        printf("Sum execution runtime:%llu\n", (unsigned long long)param.t_cputime.sum_exec_runtime);
+
+    } else if (mode == 2) {
+        struct param_inode_info param;
+        param.pid = pid;
+
+        if (ioctl(fd, IOCTL_GET_INODE_INFO, &param) == -1) {
+            perror("IOCTL_GET_INODE_INFO failed");
+            close(fd);
+            return EXIT_FAILURE;
+        }
+
+        printf("Inode number: %lu\n", param.i_info.inode_number);
+        printf("Size:         %lu\n", param.i_info.size);
+
+    } else {
+        fprintf(stderr, "Invalid mode. Use 1 for cputime, 2 for inode.\n");
+        close(fd);
+        return EXIT_FAILURE;
+    }
+
+    close(fd);
+    return EXIT_SUCCESS;
+}
